6dfb110206f3f8aaaafbed05de4e466676ef3757
Damian Korcz <damiankorcz28@gmail.com>
2024-09-21 18:51:02 +0100

    First WIP

---
+++ build/buildTheme.config.mjs
@@ -0,0 +1,12 @@
+import fs from "fs";
+import * as sass from 'sass';
+
+const inputPath = "./src/index.scss";
+const outputPath = "theme.css";
+
+const result = sass.compile(inputPath, {
+    charset: false,
+    sourceMap: false
+});
+
+fs.writeFile(outputPath, result.css, () => true);
